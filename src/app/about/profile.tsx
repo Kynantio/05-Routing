265838d1da364f9b35c578c9382ba9c9e590f12b
= <=>
2024-03-13 13:17:51 +0700

    Praktikum 1

---
+++ src/app/about/profile.tsx
@@ -0,0 +1,9 @@
+export default function Profile(){
+    <div>
+        <h1>Nama     : Kynantio Candra Abrari</h1>
+        <h1>Nim      : 2141720206</h1>
+        <h1>Kelas    : TI - 3A</h1>
+    </div>
+
+    return Profile();
+}
